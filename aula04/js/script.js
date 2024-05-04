c2f9f47d90563f92e1d9919ee7f5c0102835c72b
Kauã Almeida <kau7almeida.06@gmail.com>
2024-05-04 09:42:14 -0300

    contagem e menu de operações com while

--- aula04/js/script.js
+++ aula04/js/script.js
@@ -1,6 +1,4 @@
 var pos = 1;
-
-
 function esquerda() {
     if(pos <=1){
         pos = 1;
@@ -10,19 +8,17 @@ function esquerda() {
         pos--;
     }
     
-    pos --;
     document.getElementById("foto").innerHTML="<img src=img/foto"+pos+".png>";
 }
 
 function direita() {
     if(pos >=5){
-        pos = 1;
+        pos = 5;
     }
 
     else{
         pos++;
     }
 
-    pos ++;
     document.getElementById("foto").innerHTML="<img src=img/foto"+pos+".png>";
 }
