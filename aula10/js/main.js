049b52c7a6d422765e9aeda9a785be3ddea002cb
Kauã Almeida <kau7almeida.06@gmail.com>
2024-06-29 09:53:51 -0300

    continuaçao da aula 10 com api de filmes, estilização da estrutura e do banner

--- aula10/js/main.js
+++ aula10/js/main.js
@@ -12,42 +12,99 @@ o fetch é uma promise
 */
 
 fetch("https://api.themoviedb.org/3/movie/popular?language=pt-br&page=1&api_key=e512564992dc5dae8bd2ec2f1c1a9c90")
-.then((response)=>response.json())
-.then((dados)=>{
+    .then((response) => response.json())
+    .then((dados) => {
 
-    dados.results.map((filme)=>{
-        let cartaz = document.createElement("div");
-        cartaz.setAttribute("class", "cartaz");
-        let poster = document.createElement("img");
-        poster.src = img + filme.poster_path;
+        dados.results.map((filme) => {
+            let cartaz = document.createElement("div");
+            cartaz.setAttribute("class", "cartaz");
+            let poster = document.createElement("img");
+            poster.src = img + filme.poster_path;
 
-        let votos = document.createElement("div");
-        votos.setAttribute("class", "votos");
-        votos.innerHTML=filme.vote_average;
+            let votos = document.createElement("div");
+            votos.setAttribute("class", "votos");
+            votos.innerHTML = Math.round(filme.vote_average) + "%";
 
-        let titulo = document.createElement("h2");
-        titulo.innerText=filme.title;
+            let titulo = document.createElement("h2");
+            titulo.innerText = filme.title;
 
-        let lancamento = document.createElement("p");
-        lancamento.setAttribute("class", "lancamento");
-        lancamento.innerText=filme.release_date;
+            let lancamento = document.createElement("p");
+            lancamento.setAttribute("class", "lancamento");
 
-        cartaz.appendChild(poster);
-        esrutura.appendChild(cartaz);
-        cartaz.appendChild(votos);
-        cartaz.appendChild(titulo);
-        cartaz.appendChild(lancamento);
+            /*vamos utilizar o comando chamado substring, que nos ajuda a quebrar uma string com base na posição do caracter e a quantidade de caracteres, por exempo: 2024-06-13. para pegar os 4 primeiros digitos usamos: substring(0,4)*/
+
+            let ano = filme.release_date.substring(0, 4);
+            let mes = filme.release_date.substring(5, 7);
+            let dia = filme.release_date.substring(8, 10);
+
+            switch (mes) {
+                case "01":
+                    mes = "jan";
+                    break;
+
+                case "02":
+                    mes = "fev";
+                    break;
+
+                case "03":
+                    mes = "mar";
+                    break;
+
+                case "04":
+                    mes = "abri";
+                    break;
+
+                case "05":
+                    mes = "mai";
+                    break;
+
+                case "06":
+                    mes = "jun";
+                    break;
+
+                case "07":
+                    mes = "jul";
+                    break;
+
+                case "08":
+                    mes = "ago";
+                    break;
+
+                case "09":
+                    mes = "set";
+                    break;
+
+                case "10":
+                    mes = "out";
+                    break;
+
+                case "11":
+                    mes = "nov";
+                    break;
+
+                case "12":
+                    mes = "dez";
+                    break;
+            }
+
+            lancamento.innerText = `${dia} de ${mes} de ${ano}`;
+
+            cartaz.appendChild(poster);
+            esrutura.appendChild(cartaz);
+            cartaz.appendChild(votos);
+            cartaz.appendChild(titulo);
+            cartaz.appendChild(lancamento);
+        })
+
+        // console.log(dados.results);
+
+        // const fotos = document.querySelector("#fotos");
+
+        // for ( let i = 0; i <= 19; i++) {
+        //     fotos.innerHTML+=`<img src=${img}${dados.results[i].backdrop_path}>`;
+        // }
     })
-
-    // console.log(dados.results);
-
-    // const fotos = document.querySelector("#fotos");
-
-    // for ( let i = 0; i <= 19; i++) {
-    //     fotos.innerHTML+=`<img src=${img}${dados.results[i].backdrop_path}>`;
-    // }
-})
-.catch((erro)=>console.error(erro))
+    .catch((erro) => console.error(erro))
 
 // const cidades = ["Leme", "Guarulhos", "São Paulo", "Mauá", "Suzano"];
 
